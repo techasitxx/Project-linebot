c841a894601780a5905c6175cec3dc8b8281866e
techasitxx <40459583+techasitxx@users.noreply.github.com>
2018-06-27 13:46:31 +0700

    reply

--- line-bot.php
+++ line-bot.php
@@ -17,7 +17,7 @@ if (sizeof($request_array['events']) > 0) {
         if ($event['type'] == 'message') {
             if ($event['message']['type'] == 'text') {
                 $text = $event['message']['text'];
-                if (strpos($text,'ทั้งหมดกี่รุ่น') !== false) {
+                if (strpos($text,'กี่รุ่น') !== false) {
                     $reply_message = 'คุณต้องการถามถึงรถรุ่น Yaris หรือ Yaris ATIV?';
                 } else if (strpos($text, 'กี่รุ่นYaris')!== false) {
                     $reply_message = 'มีทั้งหมด 4 รุ่น ดังนี้ 1';
