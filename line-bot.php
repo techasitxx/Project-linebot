d81cb06801edad745b094f00188f4e234327ecca
techasitxx <40459583+techasitxx@users.noreply.github.com>
2018-07-02 12:20:28 +0700

    reply

--- line-bot.php
+++ line-bot.php
@@ -1,78 +1,67 @@
 <?php
 $ACCESS_TOKEN = 'RIo/GGcvYBSob4wVvq0qHGtCClkWJOhZMnoi7eaXrQ8ZqwfkZADtYibrhwiyA9zO42KE9KJqB7mD1FAajepmrXg8crtSjCctWY1/OjCpSD8mO6DosCZLOPvs1OxKB/gTP4EjUkCYMxxCQTTOr3r8swdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
-
 // API URL for reply message to user.
 $API_REPLY_URL = 'https://api.line.me/v2/bot/message/reply';
-
 $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
-
 if (sizeof($request_array['events']) > 0) {
-
     foreach ($request_array['events'] as $event) {
-
         // API URL for get user profile.
         $API_PROFILE_URL = 'https://api.line.me/v2/bot/profile/' . $event['source']['userId'];
-
         // Get user profile data from LINE.
         $request_profile_data = request_profile($API_PROFILE_URL, $POST_HEADER);
-
         // Reply message conditions
         $reply_message = '';
         $reply_token = $event['replyToken'];
-
         if ($event['type'] == 'message') {
             if ($event['message']['type'] == 'text') {
                 $text = $event['message']['text'];
 //                $reply_message = 'ระบบได้รับข้อความ ('.$text.') ของคุณแล้ว';
-
                 if ($text == 'รถมีทั้งหมดกี่รุ่น') {
                     $reply_message = 'คุณต้องการถามถึงรถรุ่น Yaris หรือ Yaris ATIV?';
-
                 } else if ($text == 'Yaris') {
                     $reply_message = 'มีทั้งหมด 4 รุ่น ดังนี้ 1';
-
                 } else if ($text == 'Yaris ATIV') {
                     $reply_message = 'มีทั้งหมด 5 รุ่น ดังนี้ xxxxxx';
                 } else {
 //                    $reply_message = 'User ID: ' . $event['source']['userId'] . ' type: ' . $event['source']['type'];
                     $reply_message = json_encode($event) . ' ';
                 }
-            }
-            else {
+            } else {
                 $reply_message = json_encode($event);
-
             }
+        } else if ($event['type'] == 'join') {
+            $reply_message = 'สวัสดีครับ! ผมคือผู้ช่วยของเพื่อนสมาชิก ฝากเนื้อฝากตัวด้วยนะครับ ^^ ';
+        } else if ($event['type'] == 'leave') {
+            $reply_message = 'ขอบคุณที่ให้ผมได้พบกับทุกท่าน ลาก่อนครับ';
+        } else {
+//            $reply_message = 'ระบบได้รับ Event '.ucfirst($event['type']).' ของคุณแล้ว';
+            $reply_message = json_encode($event);
         }
-//        $reply_message = $request_profile_data;
-
+        $reply_message = $request_profile_data;
         if (strlen($reply_message) > 0) {
             //$reply_message = iconv("tis-620","utf-8",$reply_message);
             $data = [
                 'replyToken' => $reply_token,
                 // Text
-               // 'messages' => [['type' => 'text', 'text' => $reply_message]]
-
+                'messages' => [['type' => 'text', 'text' => $reply_message]]
                 // Multi-Text
 //                'messages' => [
 //                    ['type' => 'text', 'text' => $reply_message],
 //                    ['type' => 'text', 'text' => 'ทดสอบ'],
 //                ],
-
                 // Image
-//                    'messages' => [[
+//                'messages' => [[
 //                    'type' => 'image',
-//                    'originalContentUrl' => 'https://simg.kapook.com/o/photow/924/kapook_world-921102.jpg',
-//                    'previewImageUrl' => 'https://simg.kapook.com/o/photow/924/kapook_world-921102.jpg',
+//                    'originalContentUrl' => 'https://i2.wp.com/beebom.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg?resize=640%2C426',
+//                    'previewImageUrl' => 'https://i2.wp.com/beebom.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg?resize=640%2C426',
 //                    'animated' => false]]
-
 //                 Sticker
 //                'messages' => [[
 //                    'type' => 'sticker',
 //                    'packageId' => '4',
 //                    'stickerId' => '623']]
-
 //                 Location
 //                'messages' => [[
 //                    'type' => 'location',
@@ -81,7 +70,6 @@ if (sizeof($request_array['events']) > 0) {
 //                    'latitude' => '13.840058',
 //                    'longitude' => '100.580857',
 //                ]]
-
                 // Template
 //                'messages' =>
 //                    [[
@@ -108,19 +96,14 @@ if (sizeof($request_array['events']) > 0) {
 //                                'text' => 'ตอบแบบสอบถามเพื่อการพัฒนาที่ดียิ่งขึ้น',
 //                            ]
 //                    ]]
-
-
             ];
             $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-
             $send_result = send_reply_message($API_REPLY_URL, $POST_HEADER, $post_body);
             echo "Result: " . $send_result . "\r\n";
         }
     }
 }
-
 echo "OK";
-
 function send_reply_message($url, $post_header, $post_body)
 {
     $ch = curl_init($url);
@@ -131,10 +114,8 @@ function send_reply_message($url, $post_header, $post_body)
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
     $result = curl_exec($ch);
     curl_close($ch);
-
     return $result;
 }
-
 function request_profile($url, $post_header)
 {
     $ch = curl_init($url);
@@ -144,8 +125,6 @@ function request_profile($url, $post_header)
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
     $result = curl_exec($ch);
     curl_close($ch);
-
     return $result;
 }
-
 ?>
