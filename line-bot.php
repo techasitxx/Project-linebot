80e46dc0a7cc548d552d7f65f4fc400458fb793d
techasitxx <40459583+techasitxx@users.noreply.github.com>
2018-07-02 11:56:11 +0700

    reply

--- line-bot.php
+++ line-bot.php
@@ -63,8 +63,8 @@ if (sizeof($request_array['events']) > 0) {
                 // Image
                     'messages' => [[
                     'type' => 'image',
-                    'originalContentUrl' => 'http://image.honghongworld.com/storage/posts/2017/09/22/ownn8x3Nb2.jpg',
-                    'previewImageUrl' => 'http://image.honghongworld.com/storage/posts/2017/09/22/ownn8x3Nb2.jpg',
+                    'originalContentUrl' => 'https://i2.wp.com/beebom.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg?resize=640%2C426',
+                    'previewImageUrl' => 'https://i2.wp.com/beebom.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg?resize=640%2C426',
                     'animated' => false]]
 
 //                 Sticker
