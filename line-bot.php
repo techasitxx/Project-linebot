caf1d84f621ad24e113ae3f08175864ca0da682a
techasitxx <40459583+techasitxx@users.noreply.github.com>
2018-07-02 14:28:39 +0700

    reply

--- line-bot.php
+++ line-bot.php
@@ -34,9 +34,15 @@ if (sizeof($request_array['events']) > 0) {
                             'animated' => false
                         ]]
                     ];
-//                    $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-//                    $send_result = send_reply_message($API_REPLY_URL, $POST_HEADER, $post_body);
-//                    echo "Result: " . $send_result . "\r\n";
+                } else if ($text == 'sticker'){
+                    $data = [
+                        'replyToken' => $reply_token,
+                        'messages' => [[
+                            'type' => 'sticker',
+                            'packageId' => '1',
+                            'stickerId' => '2584'
+                        ]]
+                    ];
                 }
             }
 
