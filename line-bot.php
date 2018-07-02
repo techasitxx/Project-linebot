733df340675e990eb877ee36b2b111b4d717dbbd
techasitxx <40459583+techasitxx@users.noreply.github.com>
2018-07-02 12:24:36 +0700

    reply

--- line-bot.php
+++ line-bot.php
@@ -39,7 +39,7 @@ if (sizeof($request_array['events']) > 0) {
 //            $reply_message = 'ระบบได้รับ Event '.ucfirst($event['type']).' ของคุณแล้ว';
             $reply_message = json_encode($event);
         }
-        $reply_message = $request_profile_data;
+//        $reply_message = $request_profile_data;
         if (strlen($reply_message) > 0) {
             //$reply_message = iconv("tis-620","utf-8",$reply_message);
             $data = [
