be0c8691cc73e800103bb7705feeb08b1673af46
Jiraphat Siriwasinchai <jiraphat.s@om4you.com>
2018-07-04 15:26:31 +0700

    -กุเอง

--- line-bot.php
+++ line-bot.php
@@ -43,6 +43,17 @@ if (sizeof($request_array['events']) > 0) {
                             'stickerId' => '2584'
                         ]]
                     ];
+                    {
+                        $data = [
+                            'replyToken' => $reply_token,
+                            'messages' => [[
+                                'type' => 'video',
+                                'originalContentUrl' => 'https://www.youtube.com/watch?v=C0DPdy98e4c',
+                                'previewImageUrl' => 'https://www.youtube.com/watch?v=C0DPdy98e4c'
+                            ]]
+                        ];
+                    }
+
                 } else if ($text == 'video') {
                     $data = [
                         'replyToken' => $reply_token,
